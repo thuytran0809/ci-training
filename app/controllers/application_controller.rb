71750b7e49b96ce15936632b1f99a7ff35297ec9
Thuy Tran <thuytest0312@gmail.com>
2017-11-21 12:18:17 +0700

    update some files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
 
-  def hello
-    render html: "hello, world!"
+  layout :layout_by_controller
+
+  protected
+
+  def layout_by_controller
+    devise_controller? ? 'devise' : 'application'
   end
 end
+
